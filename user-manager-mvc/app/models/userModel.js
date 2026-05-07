375faa2a802c837f805bdf219a7087b95cf8c15f
patedavide <paterno.davide.studente@itispaleocapa.it>
2026-05-07 06:54:27 +0000

    Aggiunta dei parametri quantitaOggetto e prezzoOggetto nella creazione di un utente e implementate le funzioni buy e sell per gestire la quantità degli oggetti.

--- user-manager-mvc/app/models/userModel.js
+++ user-manager-mvc/app/models/userModel.js
@@ -2,14 +2,28 @@
 let users = [];
 const User = {
   findAll: () => users,
-  create: (name, email, linkimg) => {
-    const newUser = { id: Date.now(), name, email, linkimg };
+  create: (name, email, linkimg, quantitaOggetto, prezzoOggetto) => {
+    const newUser = { id: Date.now(), name, email, linkimg, quantitaOggetto : Number(quantitaOggetto), prezzoOggetto };
     users.push(newUser);
     return newUser;
   },
   findById: (id) => users.find(u => u.id === parseInt(id)),
   delete: (id) => {
     users = users.filter(u => u.id !== parseInt(id));
-  }
+  },
+
+  buy: (id) => {
+    const user = User.findById(id);
+    if (user) {
+      user.quantitaOggetto++;
+    }
+  },
+
+  sell: (id) => {
+    const user = User.findById(id);
+    if (user && user.quantitaOggetto > 0) {
+      user.quantitaOggetto--;
+    }
+  }     
 };
 module.exports = User;
